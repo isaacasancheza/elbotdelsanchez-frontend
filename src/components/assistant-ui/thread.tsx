c421701eaab3bec03bc8015ec165d56e975fdc62
Isaac Sánchez <isaac@elrincondelsanchez.com>
2025-07-05 21:00:45 -0600

    :art: remove new lines

--- src/components/assistant-ui/thread.tsx
+++ src/components/assistant-ui/thread.tsx
@@ -160,11 +160,9 @@ const UserMessage: FC = () => {
   return (
     <MessagePrimitive.Root className="grid auto-rows-auto grid-cols-[minmax(72px,1fr)_auto] gap-y-2 [&:where(>*)]:col-start-2 w-full max-w-[var(--thread-max-width)] py-4">
       <UserActionBar />
-
       <div className="bg-muted text-foreground max-w-[calc(var(--thread-max-width)*0.8)] break-words rounded-3xl px-5 py-2.5 col-start-2 row-start-2">
         <MessagePrimitive.Content />
       </div>
-
       <BranchPicker className="col-span-full col-start-1 row-start-3 -mr-1 justify-end" />
     </MessagePrimitive.Root>
   )
@@ -210,9 +208,7 @@ const AssistantMessage: FC = () => {
         <MessagePrimitive.Content components={{ Text: MarkdownText }} />
         <MessageError />
       </div>
-
       <AssistantActionBar />
-
       <BranchPicker className="col-start-2 row-start-2 -ml-2 mr-2" />
     </MessagePrimitive.Root>
   )
